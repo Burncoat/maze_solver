c21ce0cd1d717b77bfe8c08204e1db81036c076f
Burncoat <htvick@gmail.com>
2025-03-13 22:13:49 -0400

    Added draw_move to Cells

--- src/cell.py
+++ src/cell.py
@@ -12,12 +12,10 @@ class Cell:
         self._y1 = None
         self._y2 = None
         self._win = win
-        self._top_left = Point(self._x1, self._y1)
-        self._top_right = Point(self._x2, self._y1)
-        self._bottom_left = Point(self._x1, self._y2)
-        self._bottom_right = Point(self._x2, self._y2)
     
     def draw(self, x1, y1, x2, y2):
+        if self._win is None:
+            return
         self._x1 = x1
         self._x2 = x2
         self._y1 = y1
@@ -34,3 +32,12 @@ class Cell:
         if self.has_bottom_wall:
             bottom_wall = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(bottom_wall, "black")
+    
+    def draw_move(self, to_cell, undo=False):
+        center_point = Point((self._x2 + self._x1)/2, (self._y2 + self._y1)/2)
+        other_center = Point((to_cell._x2 + to_cell._x1)/2, (to_cell._y2 + to_cell._y1)/2)
+        line = Line(center_point, other_center)
+        if not undo:
+            self._win.draw_line(line, "red")
+        else:
+            self._win.draw_line(line, "gray")
